6d8ff05c942216c965c45f0ae6d93db4b328f78c
Mark Okonedah <markedah9652@gmail.com>
2026-03-24 16:01:41 +0100

    update

--- app.py
+++ app.py
@@ -153,38 +153,3 @@ st.caption("Built with ❤️ using Streamlit | InfectAI v1.0 By Chisom")
 
 
 
-
-
-
-# import streamlit as st
-# from orchestrator import InfectAI
-# import json
-#
-# st.set_page_config(page_title="InfectAI", page_icon="🦠")
-#
-# st.title("🦠 InfectAI")
-# st.markdown("Intelligent Infectious Disease Risk Assessment System")
-#
-# symptoms = st.text_area("Enter patient symptoms:")
-#
-# if st.button("Assess Risk"):
-#
-#     if symptoms.strip() == "":
-#         st.warning("Please enter symptoms.")
-#     else:
-#         with st.spinner("Analyzing symptoms..."):
-#             result = InfectAI.assess(symptoms)
-#
-#         try:
-#             data = json.loads(result)
-#
-#             st.subheader("Assessment Result")
-#
-#             st.metric("Risk Level", data["Risk_Level"])
-#             st.write("**Infection Probability:**", data["Infection_Probability"])
-#             st.write("**Suspected Infection:**", data["Suspected_Infection"])
-#             st.write("**Explanation:**", data["Explanation"])
-#             st.write("**Recommended Action:**", data["Recommended_Action"])
-#
-#         except:
-#             st.error("Model returned invalid format. Check logs.")
